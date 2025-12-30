18931c3af3c1f4852c65c62a2d9ca6bf87794d68
mehras <mehrasmeydani@gmail.com>
2025-12-30 13:54:30 +0100

    compatible data type in cpp00 ex00

--- cpp_00/ex00/srcs/megaphone.cpp
+++ cpp_00/ex00/srcs/megaphone.cpp
@@ -4,12 +4,10 @@ int	main(int argc, char **argv)
 {
 	if (argc > 1)
 	{
-		for (int i = 1; i < argc; i++)
+		for (size_t i = 1; i < argc; i++)
 		{
 			for (size_t j = 0; argv[i][j]; j++)
-			{
 				argv[i][j] = (char)toupper(argv[i][j]);
-			}
 			std::cout << argv[i] << std::flush;
 		}
 		std::cout << std::endl;
