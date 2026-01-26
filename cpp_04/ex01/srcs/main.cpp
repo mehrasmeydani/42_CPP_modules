29a92a0089ff28123a1e4bbd5c02ab28a7cce580
Mehras Meydani <mehrasmeydani@gmail.com>
2026-01-26 23:52:12 +0100

    main

--- cpp_04/ex01/srcs/main.cpp
+++ cpp_04/ex01/srcs/main.cpp
@@ -28,6 +28,18 @@ int main()
 	const Animal* i = new Cat();
 	delete j;//should not create a leak
 	delete i;
+
+	const Animal* animals[10];
+	for (int i = 0; i < 10; i++) {
+		if (i % 2)
+			animals[i] = new Dog();
+		else
+			animals[i] = new Cat();
+		animals[i]->makeSound();
+	}
+	for (int i = 0; i < 10; i++) {
+		delete animals[i];
+	}
 	}
 	return 0;
 }
