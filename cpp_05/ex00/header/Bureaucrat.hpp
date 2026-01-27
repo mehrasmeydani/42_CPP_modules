e9f51297cf4782465ba10f309af30099b5a93199
Mehras Meydani <mehrasmeydani@gmail.com>
2026-01-27 02:58:31 +0100

    cpp05 ex00

---
+++ cpp_05/ex00/header/Bureaucrat.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <string>
+#include <exception>
+
+class Bureaucrat {
+	private:
+		const std::string name;
+		int	grade;
+
+	public:
+		class GradeTooHighException : public std::exception {
+			public:
+				virtual const char *what(void) const throw();
+		};
+		class GradeTooLowException : public std::exception {
+			public:
+				virtual const char *what(void) const throw();
+		};
+		const std::string&	getName(void) const;
+		int					getGrade(void) const;
+		void				setGrade(int newGrade);
+		void				incrementGrade(void);
+		void				decrementGrade(void);
+
+		Bureaucrat();
+		Bureaucrat(const Bureaucrat& in);
+		Bureaucrat(const std::string& name, int grade);
+		Bureaucrat& operator=(const Bureaucrat& in);
+		~Bureaucrat();
+};
+
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& in);
---
+++ cpp_05/ex00/srcs/Bureaucrat.cpp
@@ -0,0 +1,61 @@
+#include "../header/Bureaucrat.hpp"
+#include <iostream>
+
+const std::string&	Bureaucrat::getName(void) const {
+	return this->name;
+}
+
+int	Bureaucrat::getGrade(void) const {
+	return this->grade;
+}
+
+void	Bureaucrat::setGrade(int newGrade) {
+	if (newGrade < 1)
+		throw Bureaucrat::GradeTooHighException();
+	if (newGrade > 150)
+		throw Bureaucrat::GradeTooLowException();
+	this->grade = newGrade;
+}
+
+void	Bureaucrat::incrementGrade(void) {
+	if (getGrade() == 1)
+		throw Bureaucrat::GradeTooHighException();
+	setGrade(getGrade() - 1);
+}
+
+void	Bureaucrat::decrementGrade(void) {
+	if (getGrade() == 150)
+		throw Bureaucrat::GradeTooLowException();
+	setGrade(getGrade() + 1);
+}
+
+const char* Bureaucrat::GradeTooHighException::what(void) const throw() {
+	return "Grade too high";
+}
+
+const char* Bureaucrat::GradeTooLowException::what(void) const throw() {
+	return "Grade too low";
+}
+
+Bureaucrat::Bureaucrat(const std::string& name, int grade) : name(name), grade(grade) {
+}
+
+Bureaucrat::Bureaucrat() : name("Random dude"), grade(150) {
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& in) : name(in.getName()), grade(in.getGrade()) {
+}
+
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat& in) {
+	this->setGrade(in.getGrade());
+	return (*this);
+}
+
+Bureaucrat::~Bureaucrat() {
+}
+
+
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& in) {
+	os << in.getName() << ", bureaucrat grade " << in.getGrade() << std::endl;
+	return os;
+}
